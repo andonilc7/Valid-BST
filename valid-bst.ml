b818f39838e079ac20b5f4be7316cb36eacc37c9
andonilc7 <andonilc71@gmail.com>
2022-08-02 21:57:54 -0400

    added max and min functions

--- valid-bst.ml
+++ valid-bst.ml
@@ -1,11 +1,32 @@
 type 'a btree = Empty | Node of 'a * 'a btree * 'a btree
-
+let list = [1;2;3;4;5]
 let t1 = Node(33,
           Node(12, Empty, Empty),
           Node(74,
             Node(43, Empty, Empty),
             Empty))
 
-fun val l = 
+let rec is_max_of_list l =
+   match l with
+   | [] -> failwith "empty list"
+   | [x] -> x
+   | h::t -> max h (is_max_of_list t)
+ 
+let rec is_min_of_list l =
+   match l with
+   | [] -> failwith "empty list"
+   | [x] -> x
+   | h::t -> min h (is_min_of_list t)
+
+
+(* let rec val l = 
   match l with
-  
+  | [] -> true
+  | h1::h2::t -> 
+    if (h2>h1) then *)
+
+
+
+(* 
+- if 2nd > 1st, then check number after that, if < 
+*)
